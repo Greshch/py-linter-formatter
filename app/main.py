c30ea64ceb49bff3ab5a7265d113d713e51beee9
Greshch <greshchook@gmail.com>
2024-10-26 22:56:51 +0300

    fix code style

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ def format_linter_error(error: dict) -> dict:
         "source": "flake8",
     }
 
+
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [
@@ -20,5 +21,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        format_single_linter_file(file_path, errors) for file_path, errors in linter_report.items()
+        format_single_linter_file(file_path, errors)
+        for file_path, errors in linter_report.items()
     ]
